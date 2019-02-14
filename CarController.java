6d2539231bf51913113e5ca4954b3176cde0143a
lBillton <43410618+lBillton@users.noreply.github.com>
2019-02-14 06:58:30 +0100

    Create CarController.java

---
+++ CarController.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/*
+* This class represents the Controller part in the MVC pattern.
+* It's responsibilities is to listen to the View and responds in a appropriate manner by
+* modifying the model state and the updating the view.
+ */
+
+public class CarController {
+    // member fields:
+
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private final int delay = 50;
+    // The timer is started with an listener (see below) that executes the statements
+    // each step between delays.
+    private Timer timer = new Timer(delay, new TimerListener());
+
+    // The frame that represents this instance View of the MVC pattern
+    CarView frame;
+    // A list of cars, modify if needed
+    ArrayList<Vehicle> cars = new ArrayList<>();
+
+    //methods:
+
+    public static void main(String[] args) {
+        // Instance of this class
+        CarController cc = new CarController();
+
+        cc.cars.add(new Volvo240());
+
+        // Start a new view and send a reference of self
+        cc.frame = new CarView("CarSim 1.0", cc);
+
+        // Start the timer
+        cc.timer.start();
+    }
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+    * view to update its images. Change this method to your needs.
+    * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (Vehicle car : cars) {
+                car.move();
+                if (car.horizontal > 800 || car.horizontal < 0 || car.vertical > 500 || car.vertical < 0) {
+                    changeDirection(car);
+                    car.horizontal = //kolla varje riktning och set x och y till 800-1 / 0+1
+                }
+                int x = (int) Math.round(car.getHorizontal());
+                int y = (int) Math.round(car.getVertical());
+                frame.drawPanel.moveit(x, y);
+                // repaint() calls the paintComponent method of the panel
+                frame.drawPanel.repaint();
+            }
+        }
+    }
+
+    // Calls the gas method for each car once
+    void gas(int amount) {
+        double gas = ((double) amount) / 100;
+        for (Vehicle car : cars
+                ) {
+            car.gas(gas);
+        }
+    }
+
+    void brake(int amount){
+        double brake = ((double) amount) / 100;
+        for (Vehicle car : cars
+        ) {
+            car.brake(brake);
+        }
+    }
+
+    void stop() {
+        for (Vehicle car : cars) {
+            car.currentSpeed = 0;
+        }
+    }
+
+    void changeDirection(Vehicle car) {
+                stop();
+                car.turnLeft();
+                car.turnLeft();
+                car.startEngine();
+                car.move();
+            }
+    }
+
